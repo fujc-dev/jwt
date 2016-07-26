a91451866c377e433332a826fe7d90099abd1bc9
dane55 <328969111@qq.com>
2016-07-26 14:10:15 +0800

    ailsabe create projects

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,15 @@ var express = require('express');
 //初始化路由器Router
 var router = express.Router();
 
+const title = "GMS - 管理系统nodejs版本持续更新 version:1.0.0.010";
+
 /**
  * Get Login.ejs Page
  */
 router.get("/", function (request, response, next) {
     response.render('login.ejs', {title: title});
 });
-const  title = "GMS - 管理系统nodejs版本持续更新 version:1.0.0.010";
+
 //对网页模版进行访问,语法：response.render("/视图名称",[locals]传入模版变量,callback处理函数);
 //res.send({"username":"12346"});
 //index.ejs 此处的视图名称可以写全名称,也可以不写前缀
@@ -47,7 +49,7 @@ router.post("/homepage", function (request, response) {
     //自运行匿名函数
     (function (obj) {
         //模拟用户名与密码验证操作
-        if(obj.password == "admin" && obj.username == "admin"){
+        if (obj.password == "admin" && obj.username == "admin") {
             response.render("homepage.ejs", {title: title});
         }
         else {
@@ -55,7 +57,6 @@ router.post("/homepage", function (request, response) {
         }
     })(parameters);
 
-});;
-
+});
 /*标记此包可以被引用 */
 module.exports = router;
